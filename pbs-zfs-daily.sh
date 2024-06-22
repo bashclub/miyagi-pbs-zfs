9e312f2d877003ef4d25b7207bca5e7fe2a47eae
Chriz <christian@sysops.de>
2024-06-22 11:34:00 +0200

    Update pbs-zfs-daily.sh
    find checkzfs

--- pbs-zfs-daily.sh
+++ pbs-zfs-daily.sh
@@ -26,8 +26,10 @@ echo "zfs_auto_snapshot_label=$ZPUSHLABEL" >> /etc/bashclub/$SOURCEHOST.conf
 
 /usr/bin/bashclub-zsync -d -c /etc/bashclub/$SOURCEHOST.conf
 
+CHECKZFS=$(which checkzfs)
+
 # So one Day has 1440 Minutes, so we go condition Yellow on 1500
-/usr/local/bin/checkzfs --source $SOURCEHOST --replicafilter "$ZFSTRGT/" --filter "#$ZFSROOT/|#$ZFSSECOND/" --threshold 1500,2000 --output checkmk --prefix pull-$(hostname):$ZPUSHTAG> /tmp/cmk_tmp.out && ( echo "<<<local>>>" ; cat /tmp/cmk_tmp.out ) > /tmp/90000_checkzfs
+$CHECKZFS --source $SOURCEHOST --replicafilter "$ZFSTRGT/" --filter "#$ZFSROOT/|#$ZFSSECOND/" --threshold 1500,2000 --output checkmk --prefix pull-$(hostname):$ZPUSHTAG> /tmp/cmk_tmp.out && ( echo "<<<local>>>" ; cat /tmp/cmk_tmp.out ) > /tmp/90000_checkzfs
 
 scp /tmp/90000_checkzfs $SOURCEHOST:/var/lib/check_mk_agent/spool/90000_checkzfs_$(hostname)_$ZPOOLSRC
 
